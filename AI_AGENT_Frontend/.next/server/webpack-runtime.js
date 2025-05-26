923760a54e812583ef54562c816e17c8a5b04c82
Roseztt <rosezhaott@gmail.com>
2025-05-25 21:00:11 -0400

    minor update

--- AI_AGENT_Frontend/.next/server/webpack-runtime.js
+++ AI_AGENT_Frontend/.next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("518c6c9353c0a762")
+/******/ 		__webpack_require__.h = () => ("1c4a05e90bd97260")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
